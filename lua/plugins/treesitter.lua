feecba0ef6f3097b901372730089acc0def13509
Roshanbhadauriya <roshanbhadoriya178@gmail.com>
2025-08-31 19:48:30 +0530

    config treesitter and telescope

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,8 +1,40 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
-    vim.list_extend(opts.ensure_installed, {
+    -- core options
+    opts.auto_install = true
+    opts.highlight = vim.tbl_deep_extend("force", opts.highlight or {}, { enable = true })
+    opts.indent = vim.tbl_deep_extend("force", opts.indent or {}, { enable = true })
+
+    -- languages
+    local languages = {
+      "bash",
+      "c",
+      "cpp",
+      "css",
+      "html",
       "javascript",
-    })
+      "json",
+      "lua",
+      "markdown",
+      "python",
+      "rust",
+      "typescript",
+      "vim",
+      "vimdoc",
+    }
+
+    opts.ensure_installed = opts.ensure_installed or {}
+    vim.list_extend(opts.ensure_installed, languages)
+
+    -- dedupe ensure_installed
+    local seen, clean = {}, {}
+    for _, l in ipairs(opts.ensure_installed) do
+      if not seen[l] then
+        table.insert(clean, l)
+        seen[l] = true
+      end
+    end
+    opts.ensure_installed = clean
   end,
 }
